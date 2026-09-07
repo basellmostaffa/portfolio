efe234f76d27b6b69ae66cc4a3fb30b96efbf628
Mostafa Nour <mostafaibrahem@gmail.com>
2026-09-07 21:56:48 +0300

    Match BM mark to site blue

--- src/components/Header.js
+++ src/components/Header.js
@@ -191,7 +191,7 @@ const Header = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          <img src="/favicon.svg?v=2" alt="" />
+          <img src="/favicon.svg?v=3" alt="" />
         </Logo>
 
         <NavLinks>
